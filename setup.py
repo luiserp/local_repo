673df82706628f2d8883a210b34dba878997e24d
luiserp <luisreyesperez98@gmail.com>
2021-10-29 14:07:38 +0200

    Despliegue v 0.2.0

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import find_packages, setup
 
 setup(
     name='repopip',
-    version='0.1.1.2',
+    version='0.2.0',
     packages=find_packages(),
     include_package_data=True,
     author = 'Luis Enrique Reyes Pérez',
     author_email = 'luisreyesperez98@gmail.com',
     url = 'https://github.com/luiserp/local_repo',
     download_url = 'https://github.com/luiserp/local_repo/tarball/0.1',
-    keywords = ['repository', 'local', 'packages'],
+    keywords = ['repository', 'local', 'packages', 'pip'],
     zip_safe=False,
     install_requires=[
         'flask',
