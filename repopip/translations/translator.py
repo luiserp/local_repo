ff9679e64097aa0888892a2a44b3a79484c8fb13
luiserp <luisreyesperez98@gmail.com>
2021-11-05 07:12:54 +0100

    Bug solucionado ruta traducciones

--- repopip/translations/translator.py
+++ repopip/translations/translator.py
@@ -1,17 +1,19 @@
-import glob
+from pathlib import Path
 import json
 
 class Translator:
 
     def __init__(self):
         self.translations = { str: dict }
+        self.languages_path = Path(__file__).parent.resolve()
         self.loadLangages()
 
     def loadLangages(self):
-        languages = glob.glob("repopip/translations/*.json")
+        languages = [ lang for lang in self.languages_path.iterdir() if lang.name.endswith('json') ]
+        print('Detectando idiomas...')
         for lang in languages:
-            path = lang.split('\\')
+            print(f"idioma {lang.name} detectado")
+            path = lang.name.split('\\')
             code = path[-1].split('.')[0]
             with open(lang, encoding='utf-8') as f:
                 self.translations[code] = json.load(f)
-
